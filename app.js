da5febadb72dad89b1ca0b4e5d0b9b5e6079685d
Andrey-444 <jermandrey@gmail.com>
2024-10-07 09:36:55 -0500

    este es el primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var ciudad="quito";
+var nombre=prompt("escribe yu nombre");
+var edad=prompt("escribe tu edad");
+console.log ("hola mundo");
+
+alert (" Bienvenido " + nombre);
+
+
+
+console. log ( " Hola " + nombre + "," + " tienes " + edad + " años y vives en " + ciudad );
+
+document.write ( " Hola " + nombre + "," + " tienes " + edad + " años y vives en " + ciudad );
